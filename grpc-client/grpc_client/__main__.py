7673565df5afad4726fa5efc495e0e5d8ab673d5
ya-mori <ppap.yohei.moriya@gmail.com>
2023-10-09 01:35:55 +0900

    feat : Use schedule module

--- grpc-client/grpc_client/__main__.py
+++ grpc-client/grpc_client/__main__.py
@@ -1,8 +1,14 @@
+import time
+from os import environ
+
 from tritonclient.grpc import InferenceServerClient, InferInput, InferRequestedOutput
 from tritonclient.utils import np_to_triton_dtype
 import numpy as np
+import schedule
 
-PORT = 8001
+
+HOST = environ["GRPC_SERVER_HOST"]
+PORT = environ["GRPC_SERVER_PORT"]
 
 
 def generate_sample(num):
@@ -14,7 +20,7 @@ def generate_sample(num):
 
 def main():
     print('Hello from grpc-client')
-    with InferenceServerClient(f'localhost:{PORT}') as client:
+    with InferenceServerClient(f'{HOST}:{PORT}') as client:
         inputs = [
             InferInput(
                 name='INPUT0',
@@ -33,18 +39,26 @@ def main():
             InferRequestedOutput('OUTPUT0'),
             InferRequestedOutput('OUTPUT1'),
         ]
-        response = client.infer(
-            model_name='sample_model',
-            inputs=inputs,
-            # request_id=str(1),
-            outputs=outputs
-        )
-        # print(response.get_response())
-        print(response.get_output("OUTPUT0"))
-        print(response.get_output("OUTPUT1"))
-        print(response.as_numpy("OUTPUT0"))
-        print(response.as_numpy("OUTPUT1"))
+        try:
+            response = client.infer(
+                model_name='sample_model',
+                inputs=inputs,
+                # request_id=str(1),
+                outputs=outputs
+            )
+            # print(response.get_response())
+            print(response.get_output("OUTPUT0"))
+            print(response.get_output("OUTPUT1"))
+            print(response.as_numpy("OUTPUT0"))
+            print(response.as_numpy("OUTPUT1"))
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    main()
+    schedule.every(10).seconds.do(main)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
