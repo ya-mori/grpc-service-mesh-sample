1498e2031b18c17d87c11305c4358b407bcc0ea7
ya-mori <ppap.yohei.moriya@gmail.com>
2023-10-08 11:10:31 +0900

    feat: Add grpc-client impl

---
+++ grpc-client/bin/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t grpc-client -f Dockerfile .
---
+++ grpc-client/grpc_client/__main__.py
@@ -0,0 +1,50 @@
+from tritonclient.grpc import InferenceServerClient, InferInput, InferRequestedOutput
+from tritonclient.utils import np_to_triton_dtype
+import numpy as np
+
+PORT = 8001
+
+
+def generate_sample(num):
+    return np.asarray(
+        [[num for _ in range(16)]],
+        dtype=np.int8
+    )
+
+
+def main():
+    print('Hello from grpc-client')
+    with InferenceServerClient(f'localhost:{PORT}') as client:
+        inputs = [
+            InferInput(
+                name='INPUT0',
+                shape=[1, 16],
+                datatype=np_to_triton_dtype(np.int8)
+            ),
+            InferInput(
+                name='INPUT1',
+                shape=[1, 16],
+                datatype=np_to_triton_dtype(np.int8)
+            ),
+        ]
+        inputs[0].set_data_from_numpy(generate_sample(10))
+        inputs[1].set_data_from_numpy(generate_sample(22))
+        outputs = [
+            InferRequestedOutput('OUTPUT0'),
+            InferRequestedOutput('OUTPUT1'),
+        ]
+        response = client.infer(
+            model_name='sample_model',
+            inputs=inputs,
+            # request_id=str(1),
+            outputs=outputs
+        )
+        # print(response.get_response())
+        print(response.get_output("OUTPUT0"))
+        print(response.get_output("OUTPUT1"))
+        print(response.as_numpy("OUTPUT0"))
+        print(response.as_numpy("OUTPUT1"))
+
+
+if __name__ == '__main__':
+    main()
